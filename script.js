48616a91046fe5734faf754c83c5242e03484147
tetli <182772561+tetli@users.noreply.github.com>
2025-05-07 09:06:37 +0200

    fix

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.fake-button').forEach(btn => {
-        btn.addEventListener('click', () => {
-            alert('⚠️⚠️⚠️ YOU HAVE NOT DOWNLOADED VIRUS ⚠️⚠️⚠️');
-        });
-    });
-});
